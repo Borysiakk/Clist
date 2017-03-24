16fdb2da8e82f73cb1b58680417fd43809266ffc
Borys Szyma <szymaborys@gmail.com>
2017-03-24 10:40:27 +0100

    example

--- main.c
+++ main.c
@@ -14,6 +14,19 @@ int main()
 		printf("%i\n", (int)obj->value);
 	}
 
+	Node * a = find(list, 2);
+	Node * b = find_end(list, 3);
+	if (a != NULL)
+	{
+		printf("Found element\n");
+	}
+
+	pop_back(list);
+	for (Node * obj = list->begin; obj != NULL; obj = obj->next)
+	{
+		printf("%i\n", (int)obj->value);
+	}
+
 	DestroyList(list);
 	getchar();
 	return 0;
